97e1ba934842c5a5248194c0b06e2edc606103ff
Afonso Silva <afonso.silva@triptease.com>
2026-08-08 10:01:40 +0100

    Add Dependabot, sitemap, robots.txt, 404 page; bump yarn to 4.18

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://afonsosilva97.github.io',
+  integrations: [sitemap()],
 });
